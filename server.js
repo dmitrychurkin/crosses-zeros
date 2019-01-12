a52235f575add25bb91f9cfeb55f969a377c43c2
dmitry <dmitrychurkin1988@gmail.com>
2019-01-13 00:18:23 +0200

    optimized server static serving

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/static', express.static(path.join(__dirname, 'build', 'static')));
+app.use('/', express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname,'','build', 'favicon.ico')));
 
 app.get('/*', (req, res) => {
   if (req.path !== '/') {
