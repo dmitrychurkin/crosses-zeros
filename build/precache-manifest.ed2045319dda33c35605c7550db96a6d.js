ba572a80025de87485f11104cce99bfa1694b8cf
dmitry <dmitrychurkin1988@gmail.com>
2019-01-12 23:45:50 +0200

    added static assets for heroku

---
+++ build/precache-manifest.ed2045319dda33c35605c7550db96a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9929ac8cf107790c33c3a247c17ea9d4",
+    "url": "/static/media/mainImg.9929ac8c.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15f94105ed8c85033885",
+    "url": "/static/js/main.15f94105.chunk.js"
+  },
+  {
+    "revision": "7e4e4db9fe9f18f08dcd",
+    "url": "/static/js/1.7e4e4db9.chunk.js"
+  },
+  {
+    "revision": "15f94105ed8c85033885",
+    "url": "/static/css/main.8aa61f91.chunk.css"
+  },
+  {
+    "revision": "bb02e6530ba9ff57834b072fa28746aa",
+    "url": "/index.html"
+  }
+];
